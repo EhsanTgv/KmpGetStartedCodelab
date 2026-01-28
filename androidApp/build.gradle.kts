33451063cee807442de9ccb15b462398c0cbd122
EhsanTgv <EhsanTgv@gmail.com>
2026-01-28 10:16:42 +0300

    Implement shared module

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -61,4 +61,6 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
     debugImplementation(libs.compose.ui.tooling)
+
+    implementation(projects.shared)
 }
